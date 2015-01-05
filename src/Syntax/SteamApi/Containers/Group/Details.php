d7a7d8ed1056a6e287a46a590bea44b6589da269
Travis Blasingame <travis@Traviss-MacBook-Pro.local>
2015-01-05 11:39:51 -0600

    Adding group details avatars as url string in addition to image format

--- src/Syntax/SteamApi/Containers/Group/Details.php
+++ src/Syntax/SteamApi/Containers/Group/Details.php
@@ -1,4 +1,4 @@
-<?php  namespace Syntax\SteamApi\Containers\Group;
+<?php namespace Syntax\SteamApi\Containers\Group;
 
 class Details {
 
@@ -16,15 +16,24 @@ class Details {
 
 	public $avatarFull;
 
+	public $avatarIconUrl;
+
+	public $avatarMediumUrl;
+
+	public $avatarFullUrl;
+
 	function __construct($details)
 	{
-		$this->name         = (string)$details->groupName;
-		$this->url          = (string)$details->groupUrl;
-		$this->headline     = (string)$details->headline;
-		$this->summary      = (string)$details->summary;
-		$this->avatarIcon   = $this->getImageForAvatar((string)$details->avatarIcon);
-		$this->avatarMedium = $this->getImageForAvatar((string)$details->avatarMedium);
-		$this->avatarFull   = $this->getImageForAvatar((string)$details->avatarFull);
+		$this->name            = (string)$details->groupName;
+		$this->url             = (string)$details->groupUrl;
+		$this->headline        = (string)$details->headline;
+		$this->summary         = (string)$details->summary;
+		$this->avatarIcon      = $this->getImageForAvatar((string)$details->avatarIcon);
+		$this->avatarMedium    = $this->getImageForAvatar((string)$details->avatarMedium);
+		$this->avatarFull      = $this->getImageForAvatar((string)$details->avatarFull);
+		$this->avatarIconUrl   = (string)$details->avatarIcon;
+		$this->avatarMediumUrl = (string)$details->avatarMedium;
+		$this->avatarFullUrl   = (string)$details->avatarFull;
 	}
 
 	protected function getImageForAvatar($image)
