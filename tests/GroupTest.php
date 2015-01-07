3177cde2639d5de27e6af0e2040a6c4463885765
Travis Blasingame <stygian.warlock.v2@gmail.com>
2015-01-07 11:14:31 -0600

    Refactoring group test

--- tests/GroupTest.php
+++ tests/GroupTest.php
@@ -8,14 +8,30 @@ class GroupTest extends BaseTester {
     {
         $group = $this->steamClient->group()->GetGroupSummary($this->groupId);
 
-        // Check main properties
+        $this->checkMainSummaryProperties($group);
+        $this->checkGroupDetailProperties($group);
+        $this->checkGroupMemberProperties($group);
+        $this->checkMemberProperties($group);
+        $this->checkClasses($group);
+    }
+
+    /**
+     * @param $group
+     */
+    protected function checkMainSummaryProperties($group)
+    {
         $this->assertObjectHasAttribute('groupID64', $group);
         $this->assertObjectHasAttribute('groupDetails', $group);
         $this->assertObjectHasAttribute('memberDetails', $group);
         $this->assertObjectHasAttribute('startingMember', $group);
         $this->assertObjectHasAttribute('members', $group);
+    }
 
-        // Check group details properties
+    /**
+     * @param $group
+     */
+    protected function checkGroupDetailProperties($group)
+    {
         $this->assertObjectHasAttribute('name', $group->groupDetails);
         $this->assertObjectHasAttribute('url', $group->groupDetails);
         $this->assertObjectHasAttribute('headline', $group->groupDetails);
@@ -26,21 +42,36 @@ class GroupTest extends BaseTester {
         $this->assertObjectHasAttribute('avatarIconUrl', $group->groupDetails);
         $this->assertObjectHasAttribute('avatarMediumUrl', $group->groupDetails);
         $this->assertObjectHasAttribute('avatarFullUrl', $group->groupDetails);
+    }
 
-        // Check group group member properties
+    /**
+     * @param $group
+     */
+    protected function checkGroupMemberProperties($group)
+    {
         $this->assertObjectHasAttribute('count', $group->memberDetails);
         $this->assertObjectHasAttribute('inChat', $group->memberDetails);
         $this->assertObjectHasAttribute('inGame', $group->memberDetails);
         $this->assertObjectHasAttribute('online', $group->memberDetails);
+    }
 
-        // Check member properties
+    /**
+     * @param $group
+     */
+    protected function checkMemberProperties($group)
+    {
         $startingMember = $group->members->get($group->startingMember);
 
         $this->assertObjectHasAttribute('id32', $startingMember);
         $this->assertObjectHasAttribute('id64', $startingMember);
         $this->assertObjectHasAttribute('id3', $startingMember);
+    }
 
-        // Check classes
+    /**
+     * @param $group
+     */
+    protected function checkClasses($group)
+    {
         $this->assertInstanceOf('Syntax\SteamApi\Containers\Group', $group);
         $this->assertInstanceOf('Syntax\SteamApi\Containers\Group\Details', $group->groupDetails);
         $this->assertInstanceOf('Syntax\SteamApi\Containers\Group\MemberDetails', $group->memberDetails);
