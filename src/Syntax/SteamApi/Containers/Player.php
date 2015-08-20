b4fd2f9592cd796ef89772f5288a1ef38e683402
Ivanka Todorova <fakeheal@gmail.com>
2015-08-20 09:39:20 +0300

    Add commentPermission field.
    Steam Wiki: If set, indicates the profile allows public comments.

--- src/Syntax/SteamApi/Containers/Player.php
+++ src/Syntax/SteamApi/Containers/Player.php
@@ -48,6 +48,8 @@ class Player extends BaseContainer {
 
 	public $location;
 
+    public $commentPermission;
+
 	public function __construct($player)
 	{
 		$this->steamId                  = $player->steamid;
@@ -73,6 +75,7 @@ class Player extends BaseContainer {
 		$this->locStateCode             = $this->checkIssetField($player, 'locstatecode');
 		$this->locCityId                = $this->checkIssetField($player, 'loccityid');
 		$this->location                 = $this->getLocation();
+        $this->commentPermission        = $this->checkIssetField($player, 'commentpermission');
 	}
 
 	protected function getLocation()
