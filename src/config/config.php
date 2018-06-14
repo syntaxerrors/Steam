6fe5b544a46b66d148148cd8b5c783be3a4ae8c7
Travis Blasingame <stygian.warlock.v2@gmail.com>
2018-06-14 15:03:49 -0500

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -6,5 +6,5 @@ return array(
 	 * You can get a steam API key from http://steamcommunity.com/dev/apikey
 	 * Once you get your key, add it here.
 	*/
-	'steamApiKey' => env('STEAM_APIKEY'),
+	'steamApiKey' => env('STEAM_API_KEY'),
 );
