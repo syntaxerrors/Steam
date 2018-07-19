a682158b6548c7a4b9a6476c2597fc9f4963a7e1
Ernest E <ee@cnclub.asia>
2018-07-19 20:09:32 +0800

    feat: add package method
    This change added a Package class based on App class
    Package class have `packageDetails` method
    `packageDetails` method accepts three argument like `app()->appDetails`

---
+++ src/Syntax/SteamApi/Containers/Package.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Syntax\SteamApi\Containers;
+
+class Package extends BaseContainer
+{
+    public $id;
+    public $name;
+    public $page_image;
+    public $header;
+    public $small_logo;
+    public $apps;
+    public $page_content;
+    public $price;
+    public $platforms;
+    public $release;
+
+    public function __construct($package, $id)
+    {
+        $this->id = (int) $id;
+        $this->name = $package->name;
+        $this->apps = $package->apps;
+        $this->page_content = $this->checkIssetField($package, 'page_content', 'none');
+        $this->header = $this->checkIssetField($package, 'header_image', 'none');
+        $this->small_logo = $this->checkIssetField($package, 'small_logo', 'none');
+        $this->page_image = $this->checkIssetField($package, 'page_image', 'none');
+        $this->price = $this->formatPriceObject($package, 'price');
+        $this->platforms = $package->platforms;
+        $this->controller = $package->controller;
+        $this->release = $package->release_date;
+    }
+}
